d22d7982190c2ce7baeddf8a8c219d922dd9c02c
GeorgeSutaru <sutaru.george@gmail.com>
2018-09-26 23:03:41 +0300

    added issues endpoint. the get request returns current issues. added logic to populate the table using these issues

--- public/javascripts/table.js
+++ public/javascripts/table.js
@@ -3,7 +3,7 @@ function editTable(){
     if (editable==true){
         $("tr").each(function() {
             $(this).children("td.description").each(function(){
-                this.innerHTML = $(this).children("textarea")[0].innerHTML;
+                this.innerHTML = $(this).children("textarea")[0].value;
             })
         });
         editable=false;
@@ -20,3 +20,63 @@ function editTable(){
         editable=true;
     }
 }
+
+function loadTable() {
+
+    var table = $("#Marvel");
+
+    var base_url = window.location.origin;
+    var request = new XMLHttpRequest();
+    $.get(base_url+'/api/issues', function (data, status) {
+        // Begin accessing JSON data here
+        if (status == 'success') {
+            var headerRow = document.createElement('tr');
+            table.append(headerRow);
+            var cbColumn = document.createElement('th');
+            headerRow.append(cbColumn);
+            var cellNo = document.createElement('th');
+            cellNo.innerHTML = data.header[0];
+            headerRow.append(cellNo);
+            var description = document.createElement('th');
+            description.innerHTML = data.header[1];
+            headerRow.append(description);
+            var assignee = document.createElement('th');
+            assignee.innerHTML = data.header[2];
+            headerRow.append(assignee);
+            data.body.forEach(record => {
+                var issueClass = "description";
+                if (record[3] == true) {
+                    issueClass = "description solved";
+                }
+                var row = document.createElement('tr');
+                table.append(row);
+                var checkBox = document.createElement("INPUT");
+                checkBox.setAttribute("type", "checkbox");
+                row.append(checkBox);
+                var cellNo = document.createElement('td');
+                cellNo.className = issueClass;
+                cellNo.innerHTML = record[0];
+                row.append(cellNo);
+                var description = document.createElement('td');
+                description.innerHTML = record[1];
+                description.className = issueClass;
+                row.append(description);
+                var assignee = document.createElement('td');
+                assignee.className = issueClass;
+                assignee.innerHTML = record[2];
+                row.append(assignee);
+            });
+        } else {
+            console.log(data + ' = ' + status );
+        }
+    });
+
+
+    $("tr").each(function () {
+        $(this).children("td.description").each(function () {
+            this.innerHTML = $(this).children("textarea")[0].value;
+        })
+    });
+}
+
+loadTable();
---
+++ routes/api.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+/*router.get('/', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+    res.sendfile(path.join(__dirname, 'public','html','index.html', {title: 'The javascript web project'}));
+});*/
+
+router.get('/issues', function(req, res) {
+    res.json({
+        header: ['No', 'Task description', 'Assignee'],
+        body: [
+            [1 , 'Create page and configure git and heroku infrastructure!', 'GG', true ],
+            [2 , 'Add table basic css', 'MH', true],
+            [3, 'add button to mark issue as solved', 'MH', true],
+            [4, 'add backend data support which loads table from backend api', 'GG', true],
+            [5, 'add edit functionality to table', 'Cleo', true],
+            [6, 'create endpoints to store and retrieve table data','TBD', false],
+            [7, 'create database layer and link it to REST api middleware', 'GG', false]
+        ]});
+});
+
+module.exports = router;
